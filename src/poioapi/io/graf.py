33bbc6cc3e89ef7958924c31dfe6ad2ffc89d148
Igor <igor95n@gmail.com>
2019-04-16 13:27:38 +0300

    fixed: Conversion files when no media given in Elan file

--- src/poioapi/io/graf.py
+++ src/poioapi/io/graf.py
@@ -26,7 +26,7 @@ import graf
 
 # GrAF ID's separator
 GRAFSEPARATOR = ".."
-(TEXT, AUDIO, VIDEO, NONE) = ("text", "audio", "video", "none")
+(TEXT, AUDIO, VIDEO, NONE, UNKNOWN) = ("text", "audio", "video", "none")
 
 
 class Tier:
